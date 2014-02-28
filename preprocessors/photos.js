b8438bf5e2e142b77e1b11816b691eaf9594d826
Dylan Foster <fosterdill@gmail.com>
2014-02-27 23:29:23 -0800

    make styles responsive, refactor

--- preprocessors/photos.js
+++ preprocessors/photos.js
@@ -2,7 +2,7 @@ require('datejs');
 module.exports = function ( context ) {
 
   var data = context.resources.photos.data;
-  console.log(data);
+
   //convert dates to be human readable
   for (var i = 0; i < data.length; i++) {
     var date = Date.parse(data[i].created_time);
