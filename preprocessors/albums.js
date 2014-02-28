1d65504ddc71d517592eab7ba5c40a3e5bf5ba82
Dylan Foster <fosterdill@gmail.com>
2014-02-27 19:32:50 -0800

    add human readable dates, ignore swap files

--- preprocessors/albums.js
+++ preprocessors/albums.js
@@ -1,10 +1,20 @@
+var date = require('datejs');
 module.exports = function ( context ) {
   var data = [];
+
+  //remove all album objects which contain no photos
   for (var i = 0; i < context.resources.albums.data.length; i++) {
     if (context.resources.albums.data[i].count != undefined) {
       data.push(context.resources.albums.data[i]);
     }
   }
+
+  //convert dates to be human readable
+  for (var i = 0; i < data.length; i++) {
+    var date = Date.parse(data[i].created_time);
+    data[i].created_time = date.toString("dddd MMM d, HH:mm tt, yyyy");
+  }
+
   context.resources.albums.data = data;
   return context;
 }
